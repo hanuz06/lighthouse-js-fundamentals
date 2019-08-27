556302b8e394995f0b1c593e686a3c51905c17d8
Андрей Ли <lee1975uz@gmail.com>
2019-08-27 13:10:33 -0500

    Delete Kata5.js

--- Kata5.js
+++
@@ -1,8 +0,0 @@
-const urlEncode = function (text) {
-  //let arr = text.split(" ");
-  return text.trim().replace(/\s+/g, "%20");
-};
-
-console.log(urlEncode("Lighthouse Labs"));
-console.log(urlEncode(" Lighthouse Labs "));
-console.log(urlEncode("blue is greener than purple for sure"));
